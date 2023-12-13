a001498bf1fb10344a0b369b7ce4f0513c68e330
Colon101 <kfirgoldman12@gmail.com>
2023-12-13 03:46:19 +0200

    WHOA SO TAIM

--- index.js
+++ index.js
@@ -1,104 +1,148 @@
-const LOREM =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
 function tag(name, ...children) {
-    const result = document.createElement(name);
-    for (const child of children) {
-        if (typeof child === "string") {
-            result.appendChild(document.createTextNode(child));
-        } else {
-            result.appendChild(child);
-        }
+  const result = document.createElement(name);
+  for (const child of children) {
+    if (typeof child === "string") {
+      result.appendChild(document.createTextNode(child));
+    } else {
+      result.appendChild(child);
     }
+  }
 
-    result.att$ = function (name, value) {
-        this.setAttribute(name, value);
-        return this;
-    };
+  result.att$ = function (name, value) {
+    this.setAttribute(name, value);
+    return this;
+  };
 
-    result.onclick$ = function (callback) {
-        this.onclick = callback;
-        return this;
-    };
+  result.onclick$ = function (callback) {
+    this.onclick = callback;
+    return this;
+  };
 
-    return result;
+  return result;
 }
 
 const MUNDANE_TAGS = [
-    "canvas",
-    "h1",
-    "h2",
-    "h3",
-    "p",
-    "a",
-    "div",
-    "span",
-    "select",
-    "header",
-    "button",
-    "nav",
-    "li",
-    "ul",
-    "ol"
+  "canvas",
+  "h1",
+  "h2",
+  "h3",
+  "p",
+  "a",
+  "div",
+  "span",
+  "select",
+  "header",
+  "button",
+  "nav",
+  "li",
+  "ul",
+  "ol",
 ];
 for (let tagName of MUNDANE_TAGS) {
-    window[tagName] = (...children) => tag(tagName, ...children);
+  window[tagName] = (...children) => tag(tagName, ...children);
 }
 
 function img(src) {
-    return tag("img").att$("src", src);
+  return tag("img").att$("src", src);
 }
 
 function input(type) {
-    return tag("input").att$("type", type);
+  return tag("input").att$("type", type);
 }
-
+function iframe(src) {
+  return tag("iframe").att$("src", src);
+}
 function router(routes) {
-    let result = div();
+  let result = div();
 
-    function syncHash() {
-        let hashLocation = document.location.hash.split("#")[1];
-        if (!hashLocation) {
-            hashLocation = "/";
-        }
-
-        if (!(hashLocation in routes)) {
-            // TODO(#2): make the route404 customizable in the router component
-            const route404 = "/404";
-
-            console.assert(route404 in routes);
-            hashLocation = route404;
-        }
-
-        result.replaceChildren(routes[hashLocation]());
-
-        return result;
+  function syncHash() {
+    let hashLocation = document.location.hash.split("#")[1];
+    if (!hashLocation) {
+      hashLocation = "/";
     }
 
-    syncHash();
+    if (!(hashLocation in routes)) {
+      // TODO(#2): make the route404 customizable in the router component
+      const route404 = "/404";
 
-    // TODO(#3): there is way to "destroy" an instance of the router to make it remove it's "hashchange" callback
-    window.addEventListener("hashchange", syncHash);
+      hashLocation = route404;
+    }
 
-    result.refresh = syncHash;
+    result.replaceChildren(routes[hashLocation]());
 
     return result;
-}
+  }
 
-const navbar = nav(
-    a(img("assets/img/logo.png")),
-    a(),
-    a(),
-    a()
-).att$("class", "navbar")
-function home() {
-    return div(navbar);
+  syncHash();
+
+  // TODO(#3): there is way to "destroy" an instance of the router to make it remove it's "hashchange" callback
+  window.addEventListener("hashchange", syncHash);
+
+  result.refresh = syncHash;
+
+  return result;
 }
-function about() { }
-function overlay() { }
-function minecraft() { }
+const discordIFrame = iframe(
+  "https://discord.com/widget?id=759894401957888031&theme=dark"
+)
+  .att$("width", "500px")
+  .att$("height", "500px")
+  .att$("title", "Bridge Scrims Discord Overview")
+  .att$(
+    "sandbox",
+    "allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+  );
+const navbar = nav(
+  a(
+    img("assets/img/logo.png").att$("width", "40px").att$("height", "40px")
+  ).att$("href", "#/"),
+  ul(
+    li(a("Minecraft").att$("href", "#/minecraft")),
+    li(a("Overlays").att$("href", "#/overlays")),
+    li(a("Store").att$("href", "https://store.bridgescrims.net/")),
+    li(a("About").att$("href", "#/about"))
+  )
+).att$("class", "navbar");
+function home() {
+  return div(
+    navbar,
+    div(
+      div(
+        h1("The largest"),
+        h1("Bridge"),
+        h1("Community"),
+        h1("on ", a("Discord").att$("href", "https://discord.gg/bridgescrims"))
+      ).att$("id", "page-description"),
+      discordIFrame
+    ).att$("id", "home-container")
+  );
+}
+function about() {
+  return div(navbar, h1("you never made an about page lulw"));
+}
+function overlay() {
+  // will use an api to grab the files from gh
+  return div(navbar, h1("overlay page"));
+}
+function minecraft() {
+  return div(
+    navbar,
+    div(
+      h1("Join our feature-rich Minecraft server at"),
+      button("BridgeScrims.net").onclick$(function () {
+        navigator.clipboard.writeText("bridgescrims.net");
+      })
+    ).att$("class", "text-box")
+  );
+}
+function page404() {
+  return div(navbar, h1("404 page"));
+}
 const r = router({
-    "/": home,
-    "/about": about,
+  "/": home,
+  "/minecraft": minecraft,
+  // "/overlays": overlay,
+  // "/about": about,
+  "/404": page404,
 });
 entry.appendChild(r);
